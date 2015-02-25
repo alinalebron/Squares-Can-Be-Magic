0065a2ef641fb6c96cdd38d4f6d4777ddbd71824
ALINA LEBRON <LEBRON.ALI23@GMAIL.COM>
2015-02-25 16:49:45 -0500

    spacing / comments

--- src/MagicSquareTester.java
+++ src/MagicSquareTester.java
@@ -1,9 +1,12 @@
 /**
  * File : MagicSquareTester.java
  *
- * A
+ * A test class that creates the
+ * magic square, displays it,
+ * and lets the user know if it's magic.
  *
- * I affirm that this program is entirely my own work and none of it is the work of any other person.
+ * I affirm that this program is entirely my own work
+ * and none of it is the work of any other person.
  * Created by @AlinaLebron on 2/24/15.
  */
 public class MagicSquareTester {
@@ -11,17 +14,19 @@ public class MagicSquareTester {
 
     public static void main(String[] args) {
 
-        MagicSquare magic = new MagicSquare();
+        MagicSquare magic = new MagicSquare(); // creates new magic square object
 
-        if(magic.isMagic()) {
+        if(magic.isMagic())
+        {
 
             System.out.println(magic.toString());
 
-            System.out.println("The square is magic");
+            System.out.println("The square is magic! Awesome!");
 
         }
 
-        else {
+        else
+        {
 
             System.out.println("Sorry! This square isn't magic!");
         }
